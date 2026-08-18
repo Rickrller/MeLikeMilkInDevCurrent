96058e6106d6a9e2ec076b81b03fa6ef6593ed28
Rickrller <23185@burnside.school.nz>
2026-08-18 13:14:01 +1200

    Bugs Fixed

--- enemymovement.gd
+++ enemymovement.gd
@@ -14,7 +14,7 @@ enum enemystate {pouncestart, pouncing, parrying, everythingelse, blasted, attac
 @export var givenfruit : String
 @onready var ParryVFX = load("res://ParryBlueVFX.tscn")
 @export var sightrange : float
-@export var attackready : bool = true
+@export var attackready : bool = false
 @onready var attackcooldown = $Timer2
 @export var distancetoplayer : float
 @export var diff : Vector3
@@ -25,6 +25,8 @@ func _ready() -> void:
 		player = players[0]
 	else:
 		push_error("Error locating player")
+	await get_tree().create_timer(1).timeout
+	attackready = true
 
 func _physics_process(delta: float) -> void:
 	if state == enemystate.blasted:
--- spawner_test.gd
+++ spawner_test.gd
@@ -1,19 +1,19 @@
 extends Node3D
 
 const apple = preload("res://enemies/apple.tscn")
-const aloe = preload("res://enemies/aloevera.tscn")
+const aloe = preload("res://enemies/aloevera.tscn") # Bugs
 const coconut = preload("res://enemies/coconut.tscn")
-const strawberry = preload("res://enemies/strawberry.tscn")
+const strawberry = preload("res://enemies/strawberry.tscn") #Bugs
 const starfruit = preload("res://enemies/starfruit.tscn")
-const pear = preload("res://enemies/pearto.tscn")
+const pear = preload("res://enemies/pearto.tscn") # Bugs
 const orange = preload("res://enemies/orange.tscn")
-const lemon = preload("res://enemies/lemon.tscn")
+const lemon = preload("res://enemies/lemon.tscn") # Bugs
 const grape = preload("res://enemies/grape.tscn")
-const durian = preload("res://enemies/durian.tscn")
+const durian = preload("res://enemies/durian.tscn") # Bugs
 
 func _ready() -> void:
 	await get_tree().create_timer(2.0).timeout
-	spawn_enemy(aloe, 10, 0)
+	spawn_enemy(pear, 10, 0)
 
 func spawn_enemy(enemy_type, enemy_amount, delay) -> void:
 	var spawnshape = $SpawnArea/SpawnAreaShape
