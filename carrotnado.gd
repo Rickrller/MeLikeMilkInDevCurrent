1870f06d1d07ca762c9b0c1135767a4b2911fcce
Platche4343 <platchewastaken@gmail.com>
2026-08-23 20:33:12 +1200

    carrotnado improved

--- carrotnado.gd
+++ carrotnado.gd
@@ -1,5 +1,5 @@
 extends Area3D
-
+@export var rigidmebers : Array[Node3D] = []
 @export var members : Array[Node3D] = []
 @onready var ray = $RayCast3D
 
@@ -21,13 +21,15 @@ func _ready() -> void:
 func _physics_process(_delta: float) -> void:
 	for body in members:
 		body.velocity = 8 * body.global_position.direction_to(global_position)
-
+	for body in rigidmebers:
+		body.linear_velocity = 8 * body.global_position.direction_to(global_position)
 
 func _on_body_entered(body: Node3D) -> void:
 	if body.is_in_group("enemy"):
 		if body.is_in_group("rigidbody"):
-			return
-		members.append(body)
+			rigidmebers.append(body)
+		else:
+			members.append(body)
 
 
 func _on_body_exited(body: Node3D) -> void:
